b8d658f03583debff174726090546debce642d88
Abhiraj <abhirajsinha25@gmail.com>
2023-01-31 14:32:51 +0530

    Will be starting from imgSlider

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,5 +20,6 @@ const Container = styled.main`
     left: 0;
     right: 0;
     bottom: 0;
+    z-index: -1;
   }
 `;
---
+++ src/components/imgSlider.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function imgSlider() {
+  return (
+    <div>imgSlider</div>
+  )
+}
+
+export default imgSlider
