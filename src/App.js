8b7557847bebb16ef1b9e821408ac225d449f994
Abhiraj <abhirajsinha25@gmail.com>
2023-02-01 09:13:59 +0530

    Added routes and details component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Details from "./components/Details";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home/>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/details" element={<Details />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
