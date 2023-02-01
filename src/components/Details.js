4e13286ef092271151f7ea43497441f14e219be2
Abhiraj <abhirajsinha25@gmail.com>
2023-02-01 09:36:15 +0530

    Added title and background for comp details

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 function Details() {
   return (
-    <div>Details</div>
-  )
+    <Container>
+      <Background>
+        <img src="/images/viewers-marvel.png" />
+      </Background>
+      <ImgTitle>
+        <img src="/images/viewers-starwars.png" />
+      </ImgTitle>
+    </Container>
+  );
 }
 
-export default Details
+export default Details;
+
+const Container = styled.div`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+`;
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: -1;
+  opacity: 0.8;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const ImgTitle = styled.div`
+  height: 30vh;
+  min-height: 170px;
+  min-width: 200px;
+  width: 35vw;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+`;
